e43870b6a24bbdc2a99a92e035c529ca5452dea4
lesedi motoma <motomathato@gmail.com>
2021-09-11 16:17:21 +0200

    test: added test

---
+++ tests/match.test.ts
@@ -0,0 +1,19 @@
+import { match } from "../src/match";
+
+describe('match tests', () => {
+    const inValidinput = [
+        [null, null],
+        [undefined, undefined],
+        ["  ", ""],
+        ["1", 2],
+        ["*&", "%$@8"], 
+        ["name2", "correct"]
+    ]
+    test('Should return a string', () => {
+
+    })
+
+    test('Should return an error', () => {
+        
+    })
+})
